7f58462bf0f6ca40abf74686677c16b8eec6595f
Daniel <danicoetzee1@gmail.com>
2023-10-20 00:06:41 +0200

    Start of reporting

---
+++ Cobit-19/Business/Reports/ReportProvider.cs
@@ -0,0 +1,27 @@
+﻿using Cobit_19.Data;
+
+namespace Cobit_19.Business.Reports
+{
+    public class ReportProvider
+    {
+        private readonly AppDbContext _dbContext;
+        public ReportProvider(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public MemoryStream createDFReport()
+        {
+            return new MemoryStream();
+        }
+
+        public MemoryStream createCanvasReport()
+        {
+            return new MemoryStream();
+        }
+        public MemoryStream createObjectiveAuditReport()
+        {
+            return new MemoryStream();
+        }
+    }
+}
