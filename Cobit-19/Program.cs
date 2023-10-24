27f15d3543cd3185f5049c756afa622f1f086e0f
Gerni Visser <gerni.visser@gmail.com>
2023-10-24 08:07:22 +0200

    Restored Syncfusion version and moved licanse key to env variables

--- Cobit-19/Program.cs
+++ Cobit-19/Program.cs
@@ -59,22 +59,16 @@ builder.Services.AddScoped<FocusAreaProvider>();
 builder.Services.AddScoped<ObjectiveAuditProvider>();
 builder.Services.AddScoped<ReportProvider>();
 
+var syncfusionKey = Environment.GetEnvironmentVariable("SYNCFUSION_KEY");
 var app = builder.Build();
-
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(syncfusionKey);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    var syncfusionKey = builder.Configuration["Syncfusion:ServiceApiKey"];
-    Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(syncfusionKey);
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-else
-{
-    var key = builder.Configuration.GetValue<string>("Syncfusion");
-    Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(key);
 }
 
 
